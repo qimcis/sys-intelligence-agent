8797bde2b58562d70f5d1c9151284e38dde589c8
Chi <chixie.mcisaac@gmail.com>
2026-02-09 00:23:32 -0500

    remove vercel output

--- .vercel/output/_functions/chunks/config_BkSRtLkd.mjs
+++
@@ -1,5 +0,0 @@
-const BENCHMARK_REPO_PATH = "/home/qi/system-intelligence-benchmark";
-const COURSEEXAM_PATH = `${BENCHMARK_REPO_PATH}/benchmarks/courseexam_bench/data/raw`;
-const COURSELAB_PATH = `${BENCHMARK_REPO_PATH}/benchmarks/courselab_bench/data`;
-
-export { BENCHMARK_REPO_PATH as B, COURSEEXAM_PATH as C, COURSELAB_PATH as a };
